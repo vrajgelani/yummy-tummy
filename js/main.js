28824561b51fdec5200f4b479f77c926b40b19af
vrajgelani <vrajgelani1@gmail.com>
2026-09-10 23:05:52 +0530

    Complete Day 5 menu and food details1

--- js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", () => {
-    initializeWebsite();
-});
-
-function initializeWebsite() {
-    console.log("Yummy Tummy website initialized successfully.");
-}
