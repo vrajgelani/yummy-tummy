d697bc8fbdd150eb9aa12a32a5d9afaf5a38fb57
vrajgelani <vrajgelani1@gmail.com>
2026-09-10 15:42:31 +0530

    Day 1 Part 1 - Project Setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeWebsite();
+});
+
+function initializeWebsite() {
+    console.log("Yummy Tummy website initialized successfully.");
+}
